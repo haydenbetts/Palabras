910dfb093d941cb3a837f547eecec2dee51f91a2
Hack Reactor Student <student@hackreactor.com>
2018-10-19 13:05:41 -0700

    posting words - no redundant words

--- server/wordController.js
+++ server/wordController.js
@@ -2,7 +2,6 @@ const { Word, User } = require('../database/models.js')
 
 module.exports = {
     get: (req, res) => {
-        // should take a userid and return all words and a user id in query
         var userId = req.query.id;
 
         Word.findAll({
@@ -12,12 +11,25 @@ module.exports = {
         }).then((words) => {
             res.status(200).send(words);
         })
-
-        // res.send('hi from word get')
     },
     post: (req, res) => {
-        // should take an array of words and a user id.
-        // should findOrCreate words for that user id
+        var userId = req.body.id;
+        var words = req.body.words;
+
+        // NOTE - only works with created users
+        words.forEach(function (word, i) {
+            Word.sync().then(Word.findOrCreate({
+                where: {
+                    text: word,
+                    userId: userId
+                }
+            }).then((result) => {
+                if (i === words.length - 1) {
+                    res.status(201).send();
+                }
+            })
+            )
+        })
     },
     delete: (req, res) => {
         res.send('hi from word delete')
