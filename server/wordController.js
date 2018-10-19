37f5a7503eba4c2787ac194980754f3e6de5a88b
Hack Reactor Student <student@hackreactor.com>
2018-10-19 12:53:10 -0700

    word sending working

--- server/wordController.js
+++ server/wordController.js
@@ -1,9 +1,23 @@
+const { Word, User } = require('../database/models.js')
+
 module.exports = {
     get: (req, res) => {
-        res.send('hi from word get')
+        // should take a userid and return all words and a user id in query
+        var userId = req.query.id;
+
+        Word.findAll({
+            where: {
+                userId: userId
+            }
+        }).then((words) => {
+            res.status(200).send(words);
+        })
+
+        // res.send('hi from word get')
     },
     post: (req, res) => {
-        res.send('hi from word post')
+        // should take an array of words and a user id.
+        // should findOrCreate words for that user id
     },
     delete: (req, res) => {
         res.send('hi from word delete')
