e00f36092ee75bb96db48384fede7d2068339efc
Hack Reactor Student <student@hackreactor.com>
2018-10-19 11:56:20 -0700

    set up user and word models

---
+++ database/models.js
@@ -0,0 +1,28 @@
+const sequelize = require('./index.js');
+const Sequelize = require('sequelize');
+
+const User = sequelize.define('user', {
+    username: {
+        type: Sequelize.STRING
+    }
+});
+
+const Word = sequelize.define('word', {
+    text: {
+        type: Sequelize.STRING
+    }
+});
+
+Word.belongsTo(User);
+
+User.sync().then(() => {
+    console.log('synced user!')
+}).catch(() => {
+    console.log('failed to sync user!')
+});
+
+Word.sync().then(() => {
+    console.log('synced word!')
+}).catch(() => {
+    console.log('failed to sync word!')
+});
