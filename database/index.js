546254ac02346b8d6172dc1f49bb8d5814570ee8
haydenbetts <haydenbetts@gmail.com>
2018-12-01 19:38:43 -0800

    [progress] set up connection to heroku db

--- database/index.js
+++ database/index.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('palabras', 'root', 'hayden', {
-  host: 'localhost',
-  dialect: 'mysql'
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  "ssl": true,
+  "dialectOptions": {
+    "ssl": true
+  }
 });
 
 sequelize
