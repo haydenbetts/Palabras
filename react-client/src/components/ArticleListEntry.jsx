6f2d28c03049cf45431cb44eff4f10176f9aafe0
haydenbetts <haydenbetts@gmail.com>
2018-12-02 07:38:21 -0800

    [progress] added styling to article list

--- react-client/src/components/ArticleListEntry.jsx
+++ react-client/src/components/ArticleListEntry.jsx
@@ -5,10 +5,8 @@ const ArticleListEntry = (props) => {
   return (
     <div onClick={() => props.addWordToList()}>
       <div className="row">
-        <a href={props.article.url} className="article-title"> {props.article.title} </a>
-      </div>
-      <div className="row">
         <div className="col-md-8">
+          <a href={props.article.url} className="article-title"> {props.article.title} </a>
           <p className="publisher"> <i> {props.article.source.name} </i> </p>
           {/* TODO currently parsing away part of article content here
           This should not done on the front-end, or should at least be
@@ -17,7 +15,7 @@ const ArticleListEntry = (props) => {
           <p> {props.article.content.split("[")[0]} </p>
 
         </div>
-        <div className="col-md-4">
+        <div className="col-md-4 article-image">
           <img className="img-fluid" src={props.article.urlToImage} alt="Girl in a jacket" />
         </div>
       </div>
